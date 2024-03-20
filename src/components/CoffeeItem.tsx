ac9e6c60552f84bc536b3caa6f2b109b1de54362
David Bieregger <46626041+BierDav@users.noreply.github.com>
2024-03-20 08:16:32 +0000

    .

--- src/components/CoffeeItem.tsx
+++ src/components/CoffeeItem.tsx
@@ -1,19 +1,21 @@
 export interface Coffee {
-    title: string;
-    description: string;
-    ingredients: string[];
-    image: string;
-    id: number;
+  title: string;
+  description: string;
+  ingredients: string[];
+  image: string;
+  id: number;
 }
 
 // Array of coffees
 export type CoffeeList = Coffee[];
 
 export function CoffeeItem(props: { coffee: Coffee }) {
-    return <div style={{width: "200px  "}}>
-        <h2>{props.coffee.title}</h2>
-        <p>{props.coffee.description}</p>
-        <p>Ingredients: {props.coffee.ingredients.join(', ')}</p>
-        <img src={props.coffee.image} alt={props.coffee.title} width="200"/>
+  return (
+    <div id={"coffee-" + props.coffee.id}>
+      <h2>{props.coffee.title}</h2>
+      <p>{props.coffee.description}</p>
+      <p>Ingredients: {props.coffee.ingredients.join(", ")}</p>
+      <img src={props.coffee.image} alt={props.coffee.title} width="200" />
     </div>
+  );
 }
--- src/routes/not_working.tsx
+++ src/routes/not_working.tsx
@@ -3,30 +3,43 @@ import { createResource, For } from "solid-js";
 import { useAssets } from "solid-js/web";
 import { CoffeeItem, CoffeeList } from "~/components/CoffeeItem";
 
-
 export default function () {
+  // query a sample api
+  const [data] = createResource<CoffeeList>(async () => {
+    const resp = await fetch("https://api.sampleapis.com/coffee/hot");
+    return await resp.json();
+  });
 
-    // query a sample api
-    const [data] = createResource<CoffeeList>(async () => {
-        const resp = await fetch('https://api.sampleapis.com/coffee/hot');
-        return await resp.json();
-    })
-
-    useAssets(() => (<style>
-        {`
+  useAssets(() => (
+    <style>
+      {`
         #test-container {
         display: flex;
         flex-wrap: wrap;
         gap: 8px;
         }
         `}
-    </style>))
-    return (
-        <main>
-            <Title>Coffees - Not Working</Title>
-            <div id={"test-container"}>
-                <For each={data()}>{coffee => <CoffeeItem coffee={coffee}/>}</For>
-            </div>
-        </main>
-    );
+    </style>
+  ));
+  return (
+    <main>
+      <Title>Coffees - Not Working</Title>
+      <div id={"test-container"}>
+        <For each={data()}>
+          {(coffee) => {
+            useAssets(() => (
+              <style>
+                {`
+                        #coffee-${coffee.id} {
+                       width: 200px
+                        }
+                        `}
+              </style>
+            ));
+            return <CoffeeItem coffee={coffee} />;
+          }}
+        </For>
+      </div>
+    </main>
+  );
 }
